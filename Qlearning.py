cfa4b34d64e3139f8c4276afb894f7b79fba88fe
Nithish <nithishtimes@gmail.com>
2019-04-30 15:35:27 -0400

    changes

--- Qlearning.py
+++ Qlearning.py
@@ -14,15 +14,16 @@ class Qlearning:
     gamma  = 0.9
     reward = [0,1,5]
     epsilon = 1.0
-    min_epsilon = .01
     decay_rate = .01
     isOverFlag = False
     countA = 0
     countB = 0
     netA = 0
     netB = 0
-    def __init__(self):
+    def __init__(self, q):
         self.decay_rate = .02
+        self.Qtable = q
+        self.grid_size = 2
     # ((2n +1 )(2n+1) - 1 )/2
 
     def valid_moves(self, currentState):
@@ -32,6 +33,7 @@ class Qlearning:
         newState = updateState(currentState, action)
         max = np.argmax(self.Qtable[tuple(newState)])
         return max
+    
     def boxDiff(self, action, currentState):
         no_of_boxes = computeBoxCount(currentState)
         newState = updateState(currentState, action)
@@ -52,9 +54,7 @@ class Qlearning:
         else: 
             if diff >= 1:
                 reward = diff
-
-            
-        print('reward', reward)
+#         print('reward', reward)
         if reward == 5:
             self.isOverFlag = True
         return reward
@@ -78,37 +78,36 @@ class Qlearning:
         
         action = 0
         esp = self.epsilon
-        for i in range(100):
-            print('i:',i)
+        for batch in range(100):
             if .01 <= esp <= 1.0:
                 esp = self.decay_rate*esp
-            
-           
-            game = False
-            while not game:        
-                feasbile_moves1 = self.valid_moves(currentState)
-#                 print('iter:', i)
-                if not tuple(currentState) in self.Qtable:
-                    self.Qtable[tuple(currentState)] = np.zeros(12)
-#                     print('Qtable:', self.Qtable)
-                if np.random.uniform() < esp:
-                    action = feasbile_moves1[random.sample(range(len(feasbile_moves1)),1)[0]]
-                else:
-                    action = np.argmax(self.Qtable[tuple(currentState)])
-#                     print('1')
-#                     print('current State:', currentState)
-                newState = updateState(currentState, action)
-                              
-                if not tuple(newState) in self.Qtable:
-                    self.Qtable[tuple(newState)] = np.zeros(12)
-                self.Qtable[tuple(currentState)][action] = self.Qfunction(action, currentState)
-                currentState = newState
-                print('A:', currentState)
-                print('a count:',self.countA)
-                if isOver(currentState):
-                    currentState = np.zeros(12)
-                    game = True
-                    self.countA = 0
+        
+            for i in range(100):
+                print('i:',i)
+               
+                game = False
+                while not game:        
+                    feasbile_moves1 = self.valid_moves(currentState)
+    #                 print('iter:', i)
+                    if not tuple(currentState) in self.Qtable:
+                        self.Qtable[tuple(currentState)] = np.zeros(12)
+                    if np.random.uniform() < esp:
+                        action = feasbile_moves1[random.sample(range(len(feasbile_moves1)),1)[0]]
+                    else:
+                        action = np.argmax(self.Qtable[tuple(currentState)])
+    #                     print('current State:', currentState)
+                    newState = updateState(currentState, action)
+                    self.Qtable.setdefault(tuple(newState), np.zeros(12))
+    #                 if not tuple(newState) in self.Qtable:
+    #                     self.Qtable[tuple(newState)] = np.zeros(12)
+                    self.Qtable[tuple(currentState)][action] = self.Qfunction(action, currentState)
+                    currentState = newState
+    #                 print('A:', currentState)
+    #                 print('a count:',self.countA)
+                    if isOver(currentState,self.grid_size):
+                        currentState = np.zeros(12)
+                        game = True
+                        self.countA = 0
         print('Training Done')
         w = csv.writer(open("output.csv", "w"))
         for key, val in self.Qtable.items():
@@ -128,13 +127,11 @@ class Qlearning:
                     action2= feasbile_moves2[random.sample(range(len(feasbile_moves2)),1)[0]]
                     self.countB += self.boxDiff(action2, currentState)
                     currentState = updateState(currentState, action2)
-                    print('b count:',self.countB)
-                    print('B:', currentState)
+#                     print('b count:',self.countB)
+#                     print('B:', currentState)
                 
                 feasbile_moves1 = self.valid_moves(currentState)
-                if not tuple(currentState) in self.Qtable:
-                    self.Qtable[tuple(currentState)] = np.zeros(12)
-                actionSpace = self.Qtable[tuple(currentState)]
+                actionSpace = self.Qtable.setdefault(tuple(currentState) , np.zeros(12)) 
                 action1 = -1
                 max = 0.0
                 for i in feasbile_moves1:
@@ -144,7 +141,7 @@ class Qlearning:
                 self.countA += self.boxDiff(action1, currentState)
                 currentState = updateState(currentState, action1)
                 
-                if isOver(currentState):
+                if isOver(currentState, self.grid_size):
                     currentState = np.zeros(12)
                     game = True
                     self.decision()
@@ -154,15 +151,19 @@ class Qlearning:
                 action2= feasbile_moves2[random.sample(range(len(feasbile_moves2)),1)[0]]
                 self.countB += self.boxDiff(action2, currentState)
                 currentState = updateState(currentState, action2)
-                print('b count:',self.countB)
-                print('B:', currentState)
-                if isOver(currentState):
+#                 print('b count:',self.countB)
+#                 print('B:', currentState)
+                if isOver(currentState, self.grid_size):
                     currentState = np.zeros(12)
                     game = True
-                    self.decision()        
+                    self.decision()
+        w = csv.writer(open("output.csv", "w"))
+        for key, val in self.Qtable.items():
+            w.writerow([key, val])            
+                            
 
 if __name__ == "__main__":    
-    obj = Qlearning()
+    obj = Qlearning({})
     currentState = np.zeros(12)
     obj.epsilonGreedy(currentState)
     obj.testPlay(currentState)
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Qlearning for the game of Dots and Boxes
+
+This is not my full submission. I will upload the fully working codes as soon as possible.
